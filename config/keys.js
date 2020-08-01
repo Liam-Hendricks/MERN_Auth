a137a097dee833cab76bf70800fc68ceb5a5bbe0
Liam-Hendricks <49559330+Liam-Hendricks@users.noreply.github.com>
2020-08-01 12:04:15 +0200

    Delete keys.js

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: "mongodb+srv://application:i7HjNGc3OUjAZI12@hyperion-dev-task5.l18po.mongodb.net/Task9?retryWrites=true&w=majority",
-    secretOrKey: "secret"
-  };
